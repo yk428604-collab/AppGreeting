cc1f876e1740a728410df1ac504cf96014952e6c
yk428604-collab <yk428604@gmail.com>
2025-12-10 15:58:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('gift').addEventListener('click', function(){
+  document.getElementById('poem-container').style.display = 'block';
+  document.getElementById('gift').style.display = 'none';
+  
+});
